dafd18c986c29bc1015d00993cb3450e0280053f
Arthur Osipyan <arthurosipyan@outlook.com>
2020-03-31 13:25:04 -0400

    elevator script added, in progress

---
+++ Assets/Elevator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject movePlatform;
+
+    private void OnTriggerStay(Collider other) 
+    {
+        movePlatform.transform.position += movePlatform.transform.up * Time.deltaTime;
+    }
+}
