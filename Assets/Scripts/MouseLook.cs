b507fc9f67e9b26741f401c26b8585d1a4f007d2
Arthur Osipyan <arthurosipyan@outlook.com>
2020-03-31 00:34:52 -0400

    main lobby and player scripts

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MouseLook : MonoBehaviour
+{
+    private GameObject raycastedObj;
+    private GameObject placeHolder;
+    public GameObject RaycastedObj { get { return raycastedObj; } set { raycastedObj = value; } }
+    [SerializeField] private LayerMask layerMaskInteract;
+    // public Image uiCrosshair;
+    public Transform playerBody;
+    float xRotation = 0f;
+    // ************************************************************
+    // ********************** Sensitivity *************************
+    // ************************************************************
+    public float Sensitivity { get { return currentSensitivity; } }
+    float currentSensitivity;
+    public float maxSensitivity = 100f;
+    // public TextMeshProUGUI sensitivityTxt;
+    // ************************************************************
+    // ************************************************************
+    // ************************************************************
+
+    void Start()
+    {
+        currentSensitivity = 100f;
+        // sensitivityTxt.text = "Sensitivity: " + currentSensitivity.ToString();
+        Cursor.lockState = CursorLockMode.Locked;
+        placeHolder = new GameObject();
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * currentSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * currentSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 4f, layerMaskInteract.value))
+        {
+            RaycastedObj = hit.collider.gameObject;
+            // CrosshairActive();
+        }
+        else
+        {
+            RaycastedObj = placeHolder;
+            // CrosshairNormal();
+        }
+
+    }
+
+    // void CrosshairActive()
+    // {
+    //     uiCrosshair.color = Color.red;
+    // }
+
+    // void CrosshairNormal()
+    // {
+    //     uiCrosshair.color = Color.white;
+    // }
+
+    // public void ChangeSensitivity(float amount)
+    // {
+    //     currentSensitivity = Mathf.Clamp(currentSensitivity + amount, 0, maxSensitivity);
+    //     sensitivityTxt.text = "Sensitivity: " + currentSensitivity.ToString();
+    // }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CharacterController controller;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    Vector3 velocity;
+    // ************************************************************
+    // ************************** Speed ***************************
+    // ************************************************************
+    public float Speed { get { return currentSpeed; } }
+    float currentSpeed;
+    public float maxSpeed = 50f;
+    // public TextMeshProUGUI speedTxt;
+    // ************************************************************
+    // *************************** Jump ***************************
+    // ************************************************************
+    public float Jump { get { return currentJump; } }
+    float currentJump;
+    public float maxJump = 25f;
+    // public TextMeshProUGUI jumpTxt;
+    // ************************************************************
+    // ************************************************************
+    // ************************************************************
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    bool isGrounded;
+
     void Start()
     {
-        
+        currentSpeed = 6f;
+        // speedTxt.text = "Speed: " + currentSpeed.ToString();
+
+        currentJump = 3f;
+        // jumpTxt.text = "Jump: " + currentJump.ToString();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(currentJump * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
+
+    // public void ChangeSpeed(float amount)
+    // {
+    //     currentSpeed = Mathf.Clamp(currentSpeed + amount, 0, maxSpeed);
+    //     speedTxt.text = "Speed: " + currentSpeed.ToString();
+    // }
+
+    // public void ChangeJump(float amount)
+    // {
+    //     currentJump = Mathf.Clamp(currentJump + amount, 0, maxJump);
+    //     jumpTxt.text = "Jump: " + currentJump.ToString();
+    // }
 }
