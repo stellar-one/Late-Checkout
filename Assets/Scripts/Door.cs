a97f07fe7a23700de2a379119533213bee9781db
blackmesa94 <51970917+blackmesa94@users.noreply.github.com>
2020-04-03 17:26:36 -0400

    Adding Door animation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Animator _animator;
+    public PlayerController PC;
+
+
+
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        PC = GetComponent<PlayerController>();
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !_animator.GetBool("Open") && PC.interact == true)
+        {
+            Debug.Log("Open Door");
+
+            _animator.SetBool("Open", true);
+        }
+        else if (other.tag == "Player" && PC.interact == false)
+        {
+            Debug.Log("Close Door");
+            _animator.SetBool("Open", false);
+        }
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
