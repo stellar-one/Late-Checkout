6a8184a6ebabd7db2ef6970be2ddd91aeb3a4b1d
Arthur Osipyan <arthurosipyan@outlook.com>
2020-04-03 21:43:51 -0400

    stupid elevator

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -4,6 +4,10 @@ public class Elevator : MonoBehaviour
 {
     public GameObject movePlatform;
 
+    // firstFloor
+    // secondFloor
+    // thirdFloor
+
     private void OnTriggerStay(Collider other) 
     {
         movePlatform.transform.position += movePlatform.transform.up * Time.deltaTime;
